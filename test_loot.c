5d16f5bbfe4fb22b510cc4b72f61d5416d66f954
Frederik van der Els <fred@xpple.dev>
2025-06-23 16:22:47 +0200

    Replace tab with 4 spaces

--- test_loot.c
+++ test_loot.c
@@ -11,108 +11,108 @@ Pos findStructure(StructureConfig sconf, Generator g, SurfaceNoise sn);
 void print_loot(LootTableContext* ctx);
 
 int main() {
-	uint64_t seed = 8052710360952744907ULL;
-	int version = MC_1_21;
-	int stype = Ruined_Portal;
+    uint64_t seed = 8052710360952744907ULL;
+    int version = MC_1_21;
+    int stype = Ruined_Portal;
 
-	StructureConfig sconf;
-	if (!getStructureConfig(stype, version, &sconf)) {
-		printf("Structure config failed\n");
-		return 1;
-	}
-	Generator generator;
-	setupGenerator(&generator, version, 0);
-	applySeed(&generator, sconf.dim, seed);
-	SurfaceNoise sn;
-	initSurfaceNoise(&sn, sconf.dim, seed);
+    StructureConfig sconf;
+    if (!getStructureConfig(stype, version, &sconf)) {
+        printf("Structure config failed\n");
+        return 1;
+    }
+    Generator generator;
+    setupGenerator(&generator, version, 0);
+    applySeed(&generator, sconf.dim, seed);
+    SurfaceNoise sn;
+    initSurfaceNoise(&sn, sconf.dim, seed);
 
-	Pos pos = findStructure(sconf, generator, sn);
-	printf("Structure %s found at %d %d\n", struct2str(sconf.structType), pos.x, pos.z);
+    Pos pos = findStructure(sconf, generator, sn);
+    printf("Structure %s found at %d %d\n", struct2str(sconf.structType), pos.x, pos.z);
 
-	int biome = getBiomeAt(&generator, 4, pos.x >> 2, 320 >> 2, pos.z >> 2);
-	StructureVariant sv;
-	getVariant(&sv, sconf.structType, version, seed, pos.x, pos.z, biome);
-	StructureSaltConfig ssconf;
-	if (!getStructureSaltConfig(stype, version, sv.biome != -1 ? sv.biome : biome, &ssconf)) {
-		printf("Structure salt config failed\n");
-		return 1;
-	}
+    int biome = getBiomeAt(&generator, 4, pos.x >> 2, 320 >> 2, pos.z >> 2);
+    StructureVariant sv;
+    getVariant(&sv, sconf.structType, version, seed, pos.x, pos.z, biome);
+    StructureSaltConfig ssconf;
+    if (!getStructureSaltConfig(stype, version, sv.biome != -1 ? sv.biome : biome, &ssconf)) {
+        printf("Structure salt config failed\n");
+        return 1;
+    }
 
-	int n = END_CITY_PIECES_MAX;
-	Piece* pieces = malloc(n * sizeof(Piece));
-	int pieceCount = getStructurePieces(pieces, n, sconf.structType, ssconf, sv, version, seed, pos.x, pos.z);
-	if (pieceCount < 0) {
-		printf("Pieces not supported for structure\n");
-		free(pieces);
-		return 1;
-	}
-	printf("Piece count: %d\n", pieceCount);
-	for (int i = 0; i < pieceCount; ++i) {
-		Piece p = pieces[i];
-		printf("Piece name: %s\n", p.name);
-		if (p.chestCount == 0) {
-			continue;
-		}
-		LootTableContext ctx;
-		if (!init_loot_table_name(&ctx, p.lootTable, version)) {
-			printf("Failed to initialise loot table\n");
-			return 1;
-		}
-		for (int j = 0; j < p.chestCount; ++j) {
-			printf("Chest %d (loot seed %" PRId64 "):\n", j, p.lootSeeds[j]);
-			set_loot_seed(&ctx, p.lootSeeds[j]);
-			generate_loot(&ctx);
-			print_loot(&ctx);
-		}
-		free_loot_table_pools(&ctx);
-	}
-	free(pieces);
+    int n = END_CITY_PIECES_MAX;
+    Piece* pieces = malloc(n * sizeof(Piece));
+    int pieceCount = getStructurePieces(pieces, n, sconf.structType, ssconf, sv, version, seed, pos.x, pos.z);
+    if (pieceCount < 0) {
+        printf("Pieces not supported for structure\n");
+        free(pieces);
+        return 1;
+    }
+    printf("Piece count: %d\n", pieceCount);
+    for (int i = 0; i < pieceCount; ++i) {
+        Piece p = pieces[i];
+        printf("Piece name: %s\n", p.name);
+        if (p.chestCount == 0) {
+            continue;
+        }
+        LootTableContext ctx;
+        if (!init_loot_table_name(&ctx, p.lootTable, version)) {
+            printf("Failed to initialise loot table\n");
+            return 1;
+        }
+        for (int j = 0; j < p.chestCount; ++j) {
+            printf("Chest %d (loot seed %" PRId64 "):\n", j, p.lootSeeds[j]);
+            set_loot_seed(&ctx, p.lootSeeds[j]);
+            generate_loot(&ctx);
+            print_loot(&ctx);
+        }
+        free_loot_table_pools(&ctx);
+    }
+    free(pieces);
 
-	return 0;
+    return 0;
 }
 
 Pos findStructure(StructureConfig sconf, Generator g, SurfaceNoise sn) {
-	Pos center = {0, 0};
+    Pos center = {0, 0};
 
-	int regionSize = sconf.regionSize << 4;
-	int centerX = center.x / regionSize;
-	int centerZ = center.z / regionSize;
-	int radius = 30000000 / regionSize;
+    int regionSize = sconf.regionSize << 4;
+    int centerX = center.x / regionSize;
+    int centerZ = center.z / regionSize;
+    int radius = 30000000 / regionSize;
 
-	int x = centerX, dx = 0, z = centerZ, dz = -1;
-	const int leftBoundX = centerX - radius, rightBoundX = centerX + radius;
-	const int bottomBoundZ = centerZ - radius, topBoundZ = centerZ + radius;
-	const uint64_t max = (2ULL * radius + 1) * (2ULL * radius + 1);
-	for (uint64_t i = 0; i < max; i++) {
-		if (leftBoundX <= x && x <= rightBoundX && bottomBoundZ <= z && z <= topBoundZ) {
-			Pos p;
-			if (getStructurePos(sconf.structType, g.mc, g.seed, x, z, &p)) {
-				if (isViableStructurePos(sconf.structType, &g, p.x, p.z, 0)) {
-					if (sconf.structType == End_City ? isViableEndCityTerrain(&g, &sn, p.x, p.z) : isViableStructureTerrain(sconf.structType, &g, p.x, p.z)) {
-						return (Pos) {p.x, p.z};
-					}
-				}
-			}
-		}
-		if (x - centerX == z - centerZ || (x < centerX && x - centerX == centerZ - z) || (x > centerX && x - centerX == centerZ + 1 - z)) {
-			const int temp = dx;
-			dx = -dz;
-			dz = temp;
-		}
-		x += dx;
-		z += dz;
-	}
-	return (Pos) {0, 0};
+    int x = centerX, dx = 0, z = centerZ, dz = -1;
+    const int leftBoundX = centerX - radius, rightBoundX = centerX + radius;
+    const int bottomBoundZ = centerZ - radius, topBoundZ = centerZ + radius;
+    const uint64_t max = (2ULL * radius + 1) * (2ULL * radius + 1);
+    for (uint64_t i = 0; i < max; i++) {
+        if (leftBoundX <= x && x <= rightBoundX && bottomBoundZ <= z && z <= topBoundZ) {
+            Pos p;
+            if (getStructurePos(sconf.structType, g.mc, g.seed, x, z, &p)) {
+                if (isViableStructurePos(sconf.structType, &g, p.x, p.z, 0)) {
+                    if (sconf.structType == End_City ? isViableEndCityTerrain(&g, &sn, p.x, p.z) : isViableStructureTerrain(sconf.structType, &g, p.x, p.z)) {
+                        return (Pos) {p.x, p.z};
+                    }
+                }
+            }
+        }
+        if (x - centerX == z - centerZ || (x < centerX && x - centerX == centerZ - z) || (x > centerX && x - centerX == centerZ + 1 - z)) {
+            const int temp = dx;
+            dx = -dz;
+            dz = temp;
+        }
+        x += dx;
+        z += dz;
+    }
+    return (Pos) {0, 0};
 }
 
 void print_loot(LootTableContext* ctx) {
-	for (int i = 0; i < ctx->generated_item_count; i++) {
-		ItemStack* item_stack = &(ctx->generated_items[i]);
-		printf("%s x %d\n", get_item_name(ctx, item_stack->item), item_stack->count);
+    for (int i = 0; i < ctx->generated_item_count; i++) {
+        ItemStack* item_stack = &(ctx->generated_items[i]);
+        printf("%s x %d\n", get_item_name(ctx, item_stack->item), item_stack->count);
 
-		for (int j = 0; j < item_stack->enchantment_count; j++) {
-			EnchantInstance* ench = &(item_stack->enchantments[j]);
-			printf("    %s %d\n", get_enchantment_name(ench->enchantment), ench->level);
-		}
-	}
+        for (int j = 0; j < item_stack->enchantment_count; j++) {
+            EnchantInstance* ench = &(item_stack->enchantments[j]);
+            printf("    %s %d\n", get_enchantment_name(ench->enchantment), ench->level);
+        }
+    }
 }
